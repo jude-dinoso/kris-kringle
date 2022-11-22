53bc5218399022e92b4d1d3fa7db30dba26a0dd9
jude-dinoso <101120658+jude-dinoso@users.noreply.github.com>
2022-11-23 01:06:40 +0900

    Kris kringle Page Layout Update (#8)
    * Updated favicon & metadata
    
    * Removed customization tooltip, updated dashboard details
    
    * Updated kris kringle page
    
    Co-authored-by: Pam <patriciaannmarieortiz@gmail.com>

--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -17,15 +17,36 @@ const SamplePage = () => {
 
     return (
         <Grid container spacing={gridSpacing}>
-            <MainCard title="Welcome">
-                <span>
-                    Hi {first_name} your recipient is {recipient}
-                </span>
-                <Typography variant="body2">testaasdasdasdasdasdasdsadasdsaasdadasdasdasadasdasdas</Typography>
-            </MainCard>
             <Grid item xs={12}>
-                <Grid item xs={12} md={4}>
-                    <LoginCard isLoading={isLoading} />
+                <Grid container spacing={gridSpacing}>
+                    <Grid item lg={4} md={4} sm={12} xs={12}>
+                        <LoginCard isLoading={isLoading} />
+                    </Grid>
+                    <Grid item lg={4} md={4} sm={6} xs={12}>
+                        <MainCard title="Your monito/monita is..." sx={{ backgroundColor: '#ffb1ab', height: 270 }}>
+                            <span>Hi {first_name} </span>
+                            <Typography variant="h2">{recipient}</Typography>
+                        </MainCard>
+                    </Grid>
+                    <Grid item lg={4} md={4} sm={6} xs={12}>
+                        <MainCard title="How would you describe your monito/monita?" sx={{ backgroundColor: '#76af7b', height: 270 }}>
+                            <Typography variant="h2">textbox + button</Typography>
+                        </MainCard>
+                    </Grid>
+                    <Grid item lg={12} md={12} sm={12} xs={12}>
+                        <Grid container spacing={gridSpacing}>
+                            <Grid item sm={6} xs={12} md={6} lg={6}>
+                                <MainCard title="Your Monito/Monita's Wishlist (if any)" sx={{ backgroundColor: '#aedef0' }}>
+                                    <Typography variant="body2">get from db</Typography>
+                                </MainCard>
+                            </Grid>
+                            <Grid item sm={6} xs={12} md={6} lg={6}>
+                                <MainCard title="Your Wishlist (optional)" sx={{ backgroundColor: '#f4abb4' }}>
+                                    <Typography variant="body2">get from db</Typography>
+                                </MainCard>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </Grid>
