a6971693206aa04b545e26354c8efdcaf6e01983
Pam <patriciaannmarieortiz@gmail.com>
2022-11-23 00:57:28 +0900

    Updated kris kringle page

--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -1,20 +1,55 @@
+import { useEffect, useState } from 'react';
+
 // material-ui
 import { Typography } from '@mui/material';
+import { Grid } from '@mui/material';
+import LoginCard from 'views/dashboard/Default/LoginCard';
+import { gridSpacing } from 'store/constant';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
 
-// ==============================|| SAMPLE PAGE ||============================== //
+const SamplePage = () => {
+    const [isLoading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(false);
+    }, []);
 
-const SamplePage = () => (
-    <MainCard title="Welcome">
-        <Typography variant="body2">
-            Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
-            ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-            reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
-            qui officiate descent molls anim id est labours.
-        </Typography>
-    </MainCard>
-);
+    return (
+        <Grid container spacing={gridSpacing}>
+            <Grid item xs={12}>
+                <Grid container spacing={gridSpacing}>
+                    <Grid item lg={4} md={4} sm={12} xs={12}>
+                        <LoginCard isLoading={isLoading} />
+                    </Grid>
+                    <Grid item lg={4} md={4} sm={6} xs={12}>
+                        <MainCard title="Your monito/monita is..." sx={{ backgroundColor: '#ffb1ab', height: 270 }}>
+                            <Typography variant="h2">get from db</Typography>
+                        </MainCard>
+                    </Grid>
+                    <Grid item lg={4} md={4} sm={6} xs={12}>
+                        <MainCard title="How would you describe your monito/monita?" sx={{ backgroundColor: '#76af7b', height: 270 }}>
+                            <Typography variant="h2">textbox + button</Typography>
+                        </MainCard>
+                    </Grid>
+                    <Grid item lg={12} md={12} sm={12} xs={12}>
+                        <Grid container spacing={gridSpacing}>
+                            <Grid item sm={6} xs={12} md={6} lg={6}>
+                                <MainCard title="Your Monito/Monita's Wishlist (if any)" sx={{ backgroundColor: '#aedef0' }}>
+                                    <Typography variant="body2">get from db</Typography>
+                                </MainCard>
+                            </Grid>
+                            <Grid item sm={6} xs={12} md={6} lg={6}>
+                                <MainCard title="Your Wishlist (optional)" sx={{ backgroundColor: '#f4abb4' }}>
+                                    <Typography variant="body2">get from db</Typography>
+                                </MainCard>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
 
 export default SamplePage;
