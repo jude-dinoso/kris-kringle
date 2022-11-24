8ecf27731b44a9754fd7a7895174ea3a8004057a
Pam <patriciaannmarieortiz@gmail.com>
2022-11-24 23:57:21 +0900

    Fixed user values

--- src/views/dashboard/Default/LoginCard.js
+++ src/views/dashboard/Default/LoginCard.js
@@ -132,9 +132,9 @@ const LoginCard = ({ isLoading }) => {
                                             </MenuItem>
                                             <ListSubheader sx={{ color: 'black' }}>RSJ</ListSubheader>
                                             <MenuItem value="Nita">Nita</MenuItem>
-                                            <MenuItem value="Frances">Baby</MenuItem>
-                                            <MenuItem value="Frances">Rick</MenuItem>
-                                            <MenuItem value="Frances">Harold</MenuItem>
+                                            <MenuItem value="Baby">Baby</MenuItem>
+                                            <MenuItem value="Rick">Rick</MenuItem>
+                                            <MenuItem value="Harold">Harold</MenuItem>
                                             <MenuItem value="Jenn">Jenn</MenuItem>
                                             <MenuItem value="Grant">Grant</MenuItem>
                                             <MenuItem value="Carl">Carl</MenuItem>
@@ -152,15 +152,15 @@ const LoginCard = ({ isLoading }) => {
                                             <MenuItem value="Sean">Sean</MenuItem>
                                             <MenuItem value="Martina">Martina</MenuItem>
                                             <MenuItem value="Rocco">Rocco</MenuItem>
-                                            <MenuItem value="Hansel">Zeny</MenuItem>
-                                            <MenuItem value="Zeny">Hansel</MenuItem>
+                                            <MenuItem value="Zeny">Zeny</MenuItem>
+                                            <MenuItem value="Hansel">Hansel</MenuItem>
                                             <MenuItem value="Hazel">Hazel</MenuItem>
                                             <MenuItem value="Mark Liriel">Mark Liriel</MenuItem>
                                             <MenuItem value="Noah">Noah</MenuItem>
                                             <MenuItem value="Zoe">Zoe</MenuItem>
                                             <MenuItem value="Sachi">Sachi</MenuItem>
                                             <MenuItem value="Frances">Frances</MenuItem>
-                                            <MenuItem value="Frances">Richard</MenuItem>
+                                            <MenuItem value="Richard">Richard</MenuItem>
                                             <MenuItem value="Vincent">Vincent</MenuItem>
                                             <MenuItem value="Charlize Kate">Charlize Kate</MenuItem>
                                             <MenuItem value="Mark Haze">Mark Haze</MenuItem>
